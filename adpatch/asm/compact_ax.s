4999b62700ba702b73035d3545ab251d40fbb400
Peter De Wachter <pdewacht@gmail.com>
2017-12-02 21:22:33 +0100

    Save two bytes in compact_*.s

--- adpatch/asm/compact_ax.s
+++ adpatch/asm/compact_ax.s
@@ -1,25 +1,23 @@
 ; receives input on stack
 ; modifies ax, cx, dx
-; 31 bytes
+; 29 bytes
 
-	mov dx, PORT
-	mov cl, 13
+        mov cl, 13
 L1:
-	out dx, al
-	inc dx
-	inc dx
+        mov dx, PORT
+        out dx, al
+        inc dx
+        inc dx
         mov al, cl
-	out dx, al
+        out dx, al
         sub al, 4
-	out dx, al
+        out dx, al
         add al, 4
-	out dx, al
-	mov ch, 0x22
+        out dx, al
+        mov ch, 0x22
 L2:     in al, dx
         dec ch
         jnz L2
-	dec dx
-	dec dx
         mov al, ah
-	dec cx
-	jpe L1
+        dec cx
+        jpe L1
--- adpatch/asm/compact_st.s
+++ adpatch/asm/compact_st.s
@@ -1,26 +1,24 @@
 ; receives input on stack
 ; modifies ax, cx, dx, sp
 ; does not modify the stack data (softdisk relies on this!)
-; 30 bytes
+; 28 bytes
 
-	mov dx, PORT
-	mov cl, 13
+        mov cl, 13
 L1:
-	pop ax
-	out dx, al
-	inc dx
-	inc dx
+        pop ax
+        mov dx, PORT
+        out dx, al
+        inc dx
+        inc dx
         mov al, cl
-	out dx, al
+        out dx, al
         sub al, 4
-	out dx, al
+        out dx, al
         add al, 4
-	out dx, al
-	mov ah, 0x22
+        out dx, al
+        mov ah, 0x22
 L2:     in al, dx
         dec ah
         jnz L2
-	dec dx
-	dec dx
-	dec cx
-	jpe L1
+        dec cx
+        jpe L1
