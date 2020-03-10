820bb1bd5295022190a54d7da3df8fdb84994625
Peter De Wachter <pdewacht@gmail.com>
2020-03-10 22:16:59 +0100

    Reduce EXE size

--- opl2test/build.sh
+++ opl2test/build.sh
@@ -9,11 +9,11 @@ DEFS="-dVERSION=$VERSION"
 set -x
 $CXX $DEFS -fo=opl2main main.cpp
 $CXX $DEFS -fo=opl3main -dOPL3 main.cpp
-$CXX $DEFS OPL2.cpp
+$CXX $DEFS -zm OPL2.cpp
 $CXX $DEFS demotune.cpp
 $CXX $DEFS timer.cpp
-wlink name opl2test system dos file opl2main,OPL2,demotune,timer option quiet,map
-wlink name opl3test system dos file opl3main,OPL2,demotune,timer option quiet,map
+wlink name opl2test system dos file opl2main,OPL2,demotune,timer option eliminate,quiet,map
+wlink name opl3test system dos file opl3main,OPL2,demotune,timer option eliminate,quiet,map
 
 $CXX $DEFS oplreset.cpp
-wlink name oplreset system dos file oplreset,OPL2 option quiet,map
+wlink name oplreset system dos file oplreset,OPL2 option eliminate,quiet,map
